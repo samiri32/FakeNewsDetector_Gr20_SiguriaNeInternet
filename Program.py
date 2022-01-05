95759f988a47bb59078b9640cf7c9c8a6846a6ee
samiri32 <58012577+samiri32@users.noreply.github.com>
2022-01-05 15:15:05 +0100

    ndryshime t'vogla qe munden me ndihmu n'fazen fillestare

--- Program.py
+++ Program.py
@@ -12,3 +12,10 @@ import nltk
 nltk.download('stopwords')
 
 print(stopwords.words('albanian'))
+
+news_dataset=pd.read_csv(r"C:\Users\samir\OneDrive\Desktop\ProjektiSiguriNeInternet\faza3\alb-fake-news-corpus\full_texts\fake\1.txt", engine="python")
+#print(news_dataset.shape)
+print(news_dataset.isnull().sum())
+
+#news_dataset['content']= news_dataset['id']+' '+news_dataset['title']
+#print(news_dataset['content'])
